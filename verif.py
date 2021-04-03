81d32e584fcf8e59207a605ae7db5d073588639e
Dems <tom.didier4@gmail.com>
2021-04-03 11:15:51 +0200

    verif.py et testDet.sh fixes
    testDet.sh donne désormais correctement les fichiers à vérifier à verif.py
    verif.py a été optimisé et annonce désormais si l'un des fichiers n'a pas pu être ouvert/lu
    
    Note: la déterminisation a eu une réponse positive au test

--- verif.py
+++ verif.py
@@ -7,29 +7,29 @@ if len(sys.argv) <= 3:
 try:
     file1 = open(sys.argv[1], "r")
     file2 = open(sys.argv[2], "r")
-
-    linesF1 = file1.readlines()
-    linesF2 = file2.readlines()
-
-    file1.close()
-    file2.close()
-
-    if linesF1 == linesF2:
-        print(sys.argv[3] + " has been successfully minimised/determinised!")
-    else:
-        print(sys.argv[3] + " minimisation/determinisation has failed!")
-        err = 0
-        for i in range(0, len(linesF1)):
-            if i >= len(linesF2):
-                err += len(linesF1) - len(linesF2)
-                print(str(err) + " errors found")
-                sys.exit()
-            if linesF1[i] != linesF2[i]:
-                err+=1
-
-        if len(linesF2) > len(linesF1):
-            err += len(linesF2) - len(linesF1)
-
-        print(str(err) + " errors found")
 except:
-    sys.exit()
+    sys.exit("Could not openned one or both given files")
+
+linesF1 = file1.readlines()
+linesF2 = file2.readlines()
+
+file1.close()
+file2.close()
+
+if linesF1 == linesF2:
+    print(sys.argv[3] + " has been successfully minimised/determinised!")
+else:
+    print(sys.argv[3] + " minimisation/determinisation has failed!")
+    err = 0
+    for i in range(0, len(linesF1)):
+        if i >= len(linesF2):
+            err += len(linesF1) - len(linesF2)
+            print(str(err) + " errors found")
+            sys.exit()
+        if linesF1[i] != linesF2[i]:
+            err+=1
+
+    if len(linesF2) > len(linesF1):
+        err += len(linesF2) - len(linesF1)
+
+    print(str(err) + " errors found")
